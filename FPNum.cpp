382fda22abc9efab369aa8161d47d32ebef9be8f
马浩程 <mhc@mahaochengdeMBP.lan>
2015-12-24 22:18:52 +0800

    org with Ascii Art

---
+++ FPNum.cpp
@@ -0,0 +1,7 @@
+//
+// Created by 马浩程 on 15/12/24.
+//
+
+#include "FPNum.h"
+
+int FPNum::accuracy = 20;
---
+++ FPNum.h
@@ -0,0 +1,41 @@
+//
+// Created by 马浩程 on 15/12/24.
+//
+
+#ifndef ARCTAN_FPNUM_H
+#define ARCTAN_FPNUM_H
+
+
+#include <sys/_types/_int32_t.h>
+
+
+class FPNum
+{
+    static int accuracy;
+private:
+    int32_t *intPart,*decPart;
+    int intL,decL;
+    bool sign;
+
+
+public:
+    FPNum()
+    {
+        sign = true;
+        intL=1;
+        decL=accuracy/9+1;
+        intPart=new int32_t[intL+decL];
+    }
+
+
+
+    ~FPNum()
+    {
+        if(intPart)
+            delete[] intPart;
+    }
+
+};
+
+
+#endif //ARCTAN_FPNUM_H
